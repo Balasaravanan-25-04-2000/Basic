3565715cf63f761f8db0698b78614d37562e5d54
Balasaravanan-25-04-2000 <86124542+Balasaravanan-25-04-2000@users.noreply.github.com>
2021-06-28 21:53:07 +0530

    Update div_by_5.py

--- div_by_5.py
+++ div_by_5.py
@@ -1,6 +1,12 @@
-x = int(input('enter a number'))
+#To find weather the entered number is Divisible by 5 or not
+x = int(input('enter a number:'))
 y = x % 5
 if y == 0:
     print('Divisible by 5')
 else:
     print("Not Divisible by 5")
+#Output:
+#enter a number:
+#Divisible by 5
+     #OR
+#Not Divisible by 5
