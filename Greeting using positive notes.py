dfcfb37f9afa6427adb1117b1706fad002b5f15e
Balasaravanan-25-04-2000 <86124542+Balasaravanan-25-04-2000@users.noreply.github.com>
2021-06-29 23:01:52 +0530

    Create Greeting using positive notes.py

---
+++ Greeting using positive notes.py
@@ -0,0 +1,20 @@
+#To Greet an individual using positive notes
+x = int(input('Enter any number from 1 to 5:'))
+if x == 1:
+    print('Good morning handsome!',
+    'Have a great day.')
+if x == 2:
+    print('What"s up buddy?',
+    'Hope you"re doing great.')
+if x == 3:
+    print('Hello friend!',
+    'You have a charming smile.')
+if x == 4:
+    print('Hi dude!',
+    'Let your day be filled with Happiness!')
+if x == 5:
+    print('Welcome Genius!',
+    'All the best for the tasks you"re upto.')
+#Output:
+#Enter any number from 1 to 5:4
+#Hi dude! Let your day be filled with Happiness!
